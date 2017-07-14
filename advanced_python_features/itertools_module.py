ddf519c739778bec1b5fc95c290a95f57a092fc7
bgroveben <bgroveben@gmail.com>
2017-07-13 23:52:33 -0700

    Finish itertools module

--- advanced_python_features/itertools_module.py
+++ advanced_python_features/itertools_module.py
@@ -9,3 +9,22 @@ for p in permutations([1, 2, 3]):
     print(p)
 
 print()
+
+# Similarly, combinations generates all of the possible ways of selecting items
+# from a collection.
+# Unlike permutations, the order does not matter.
+from itertools import combinations
+for c in combinations([1, 2, 3, 4], 2):
+    print(c)
+
+print()
+
+# itertools also contains utitlity functions such as chain, which takes
+# iterables and creates a new iterator that returns elements from the given
+# iterables sequentially, as a single sequence.
+from itertools import chain
+
+for c in chain(range(3), range(12, 15)):
+    print(c)
+
+print()
