a10f4363c8225a183e22d35ea52cc1d4fc378691
bgroveben <bgroveben@gmail.com>
2017-12-06 19:49:55 -0800

    Create test file for Python bytecode experiments

---
+++ menagerie/adding.py
@@ -0,0 +1,9 @@
+################################################################################
+# This little file will help me to better understand how Python bytecode works.
+################################################################################
+
+def add(a, b):
+    # print(a + b)
+    return a + b
+
+add(1, 2)
