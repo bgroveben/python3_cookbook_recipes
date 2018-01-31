af3986993a745874f03af2e91cbdb394f728e7a9
bgroveben <bgroveben@gmail.com>
2018-01-31 13:50:25 -0800

    Pytest project -- Refactoring our Tests with Fixtures

--- pytest_project/wallet/test_wallet.py
+++ pytest_project/wallet/test_wallet.py
@@ -10,25 +10,35 @@ from wallet import Wallet, InsufficientAmount
 # It will be modeled as a class with two instance methods: spend_cash and
 # add_cash.
 
-def test_default_initial_amount():
-    wallet = Wallet()
-    assert wallet.balance == 0
+# Refactor our previous tests to use test fixtures where appropriate.
+@pytest.fixture
+def empty_wallet():
+    """
+    Returns a Wallet instance with a zero balance.
+    """
+    return Wallet()
 
-def test_setting_initial_amount():
-    wallet = Wallet(100)
+@pytest.fixture
+def wallet():
+    """
+    Returns a Wallet instance with a balance of 20.
+    """
+    return Wallet(20)
+
+def test_default_initial_amount(empty_wallet):
+    assert empty_wallet.balance == 0
+
+def test_setting_initial_amount(wallet):
+    assert wallet.balance == 20
+
+def test_wallet_add_cash(wallet):
+    wallet.add_cash(80)
     assert wallet.balance == 100
 
-def test_wallet_add_cash():
-    wallet = Wallet(10)
-    wallet.add_cash(90)
-    assert wallet.balance == 100
-
-def test_wallet_spend_cash():
-    wallet = Wallet(20)
+def test_wallet_spend_cash(wallet):
     wallet.spend_cash(10)
     assert wallet.balance == 10
 
-def test_wallet_insufficient_amount_exception():
-    wallet = Wallet()
+def test_wallet_insufficient_amount_exception(empty_wallet):
     with pytest.raises(InsufficientAmount):
-        wallet.spend_cash(100)
+        empty_wallet.spend_cash(100)
