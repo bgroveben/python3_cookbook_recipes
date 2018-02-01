f46372c64981b0e4011ccb6008da5fc86cbeacb1
bgroveben <bgroveben@gmail.com>
2018-02-01 10:56:50 -0800

    Parametrizing test functions to facilitate different test inputs

--- pytest_project/wallet/test_wallet.py
+++ pytest_project/wallet/test_wallet.py
@@ -11,6 +11,10 @@ from wallet import Wallet, InsufficientAmount
 # add_cash.
 
 # Refactor our previous tests to use test fixtures where appropriate.
+# It is a good practice to add docstrings for your fixtures.
+# To see all the available fixtures, run the following command:
+# [=> pytest --fixtures
+
 @pytest.fixture
 def empty_wallet():
     """
@@ -42,3 +46,30 @@ def test_wallet_spend_cash(wallet):
 def test_wallet_insufficient_amount_exception(empty_wallet):
     with pytest.raises(InsufficientAmount):
         empty_wallet.spend_cash(100)
+
+
+# Having tested the individual methods in the Wallet class, the next step we
+# should take is to test various combinations of these methods.
+# This is to answer questions such as "If I have an initial balance of 30, and
+# spend 20, then add 100, and later on spend 50, how much should the balance
+# be?"
+# As you can imagine, writing out those steps in the tests would be tedious,
+# and pytest provides quite a delightful solution: Parametrized test functions.
+# To capture a scenario like the one above, we can write a test:
+
+"""
+This enables us to test different scenarios, all in one function.
+We make use of the @pytest.mark.parametrize decorator, where we can specify the
+names of the arguments that will be passed to the test function, and a list of
+arguments corresponding to the names.
+"""
+@pytest.mark.parametrize("earned, spent, expected", [
+    (30, 10, 20),
+    (20, 2, 18),
+])
+
+def test_transactions(earned, spent, expected):
+    my_wallet = Wallet()
+    my_wallet.add_cash(earned)
+    my_wallet.spend_cash(spent)
+    assert my_wallet.balance == expected
