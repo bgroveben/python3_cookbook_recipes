b2139ab80267eff4d2739715065a01a33f563c4c
bgroveben <bgroveben@gmail.com>
2018-03-14 03:16:07 -0700

    Requests, tokens, and signal handlers, oh my

--- flask_api_security/exceptions.py
+++ flask_api_security/exceptions.py
@@ -1,6 +1,7 @@
 class HttpException(Exception):
 
     # -> https://www.python.org/dev/peps/pep-3107/
+    # -> https://www.python.org/dev/peps/pep-0484/
     def __init__(self, message, code=500) -> None:
         super().__init__(message)
         self.code = code
--- flask_api_security/signals.py
+++ flask_api_security/signals.py
@@ -11,3 +11,78 @@ from hashlib import sha256
 from werkzeug.contrib.cache import BaseCache
 
 from exceptions import HttpException
+
+
+def get_token_from_request(request: Request):
+    header = request.headers.get('Authentication', u'')
+    regex = re.compile(u"^Bearer (?P<token>.*)$")
+    matches = regex.match(header)
+    if matches is None:
+        token = None
+    else:
+        token = matches.groupdict({u'token': None}).get(u'token)
+    return token
+
+
+class TokenSignalHandler:
+
+    def __init__(self, keys) -> None:
+        self._keys = keys
+
+        # https://www.digitalocean.com/community/tutorials/how-to-use-args-and-kwargs-in-python-3
+    def request_started_handler(self, sender, **extra):
+        request.jwt_claims = {
+            'jti': uuid1(),
+            'iss': 'public',
+            'aud': 'example-app'
+        }
+
+    def request_finished_handler(self, sender, response, **extra):
+        now = int(time())
+        claims = {
+            'jti': request.jwt_claims['jti'],
+            'iat': now,
+            'nbf': now,
+            'exp': now,
+            'iss': request.jwt_claims['aud'],
+            'aud': request.jwt_claims['iss'],
+            'response': {
+                'status_code': response.status_code,
+                'body_hash_alg': 'S512',
+                'body_hash': sha512(response.data).hexidigest()
+            }
+        }
+        jws = JWS(json.dumps(claims), alg="HS256")
+        signed_content = jws.sign_compact(keys=self._keys)
+        response.headers['X-JWT'] = signed_content
+
+
+class EncryptionSignalHandler:
+
+    def __init__(self, keys: List[Key]) -> None:
+        self._keys = keys
+
+    def request_started_handler(self, sender, **extra):
+        if request.content_type == u'application/jose:
+            jwe = JWE()
+            decrypted = jwe.decrypt(request.body, self._keys)
+
+    def request_finished_handler(self, sender, response, **extra):
+        if response.content_type == 'applicatin/json':
+            jwe = JWE(str(response.data), alg='A256KW', enc='A256CBC-HS512', cty='application/json')
+            encrypted = jwe.encrypt(self._keys, kid=self._keys[0].kid)
+            response.content_type = 'application/jose'
+            response.data = encrypted
+
+
+class ReplayPreventionSignalHandler:
+
+    def __init__(self, cache: BaseCache) -> None:
+        self.__cache = cache
+
+    def request_started_handler(self, sender, **extra):
+        token = get_token_from_request(request)
+        if token is None:
+            raise HttpException("Authorization required", 401)
+        if not self.__cache.add(sha512(token.encode('utf-8')), 1):
+            raise HttpException("Invalid Request", 400)
