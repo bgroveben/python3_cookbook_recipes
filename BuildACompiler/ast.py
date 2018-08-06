940c4f80f53c55eed42e6beb0c63094bbd792fbc
bgroveben <bgroveben@gmail.com>
2018-08-06 01:41:18 -0700

    Write the file that builds the abstract syntax tree

---
+++ BuildACompiler/ast.py
@@ -0,0 +1,25 @@
+class Number():
+    def __init__(self, value):
+        self.value = value
+
+    def eval(self):
+        return int(self.value)
+
+
+class BinaryOp():
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+
+class Sum(BinaryOp):
+    def eval(self):
+        return self.left.eval() - self.right.eval()
+
+
+class Print():
+    def __init__(self, value):
+        self.value = value
+
+    def eval(self):
+        print(self.value.eval())
