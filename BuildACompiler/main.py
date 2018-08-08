7b853aabea571cd5cabbd83499cf416fd8b1d987
bgroveben <bgroveben@gmail.com>
2018-08-08 16:28:03 -0700

    Update the compiler's main file to accommodate the code generator

--- BuildACompiler/main.py
+++ BuildACompiler/main.py
@@ -1,14 +1,24 @@
 from lexer import Lexer
 from parser import Parser
+from codegen import CodeGen
 
-text_input = """
-print(4 + 4 - 2);
-"""
+
+fname = "input.toy"
+with open(fname) as f:
+    text_input = f.read()
 
 lexer = Lexer().get_lexer()
 tokens = lexer.lex(text_input)
+codegen = CodeGen()
+
+module = codegen.module
+builder = codegen.builder
+printf = codegen.printf
 
 pg = Parser()
 pg.parse()
 parser = pg.get_parser()
 parser.parse(tokens).eval()
+
+coedgen.create_ir()
+codegen.save_ir("output.ll")
