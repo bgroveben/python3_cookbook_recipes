12c89323a52422a6cd075566b3af6b03b57c9ae5
bgroveben <bgroveben@gmail.com>
2018-09-20 00:30:18 -0700

    Write Countdown module to be used with pickling experiments

---
+++ python-cookbook-3ed/countdown.py
@@ -0,0 +1,21 @@
+import time
+import threading
+
+class Countdown:
+    def __init__(self, n):
+        self.n = n
+        self.thr = threading.Thread(target=self.run)
+        self.thr.daemon = True
+        self.thr.start()
+
+    def run(self):
+        while self.n > 0:
+            print('T-minus', self.n)
+            self.n -= 1
+            time.sleep(5)
+
+    def __getstate__(self):
+        return self.n
+
+    def __setstate__(self, n):
+        self.__init__(n)
